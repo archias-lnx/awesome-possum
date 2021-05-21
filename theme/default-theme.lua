0c66ba3856fb614db29bed41f8c539453e741edf
Matthias <76154169+archias-lnx@users.noreply.github.com>
2021-05-21 13:59:23 +0200

    Simplified Titlebar Icontheme

--- theme/default-theme.lua
+++ theme/default-theme.lua
@@ -71,92 +71,81 @@ theme.titlebar_fg_focus = '#000000' .. '66'
 theme.titlebar_fg_normal = '#000000' .. '66'
 
 -- Close Button
-theme.titlebar_close_button_normal = tip .. 'close_normal.svg'
-theme.titlebar_close_button_focus = tip .. 'close_focus.svg'
+local close_color = 'red'
+theme.titlebar_close_button_normal = tip .. 'black_normal.svg'
+theme.titlebar_close_button_focus = tip .. close_color .. '_normal.svg'
+-- Hovered Close Button
+theme.titlebar_close_button_normal_hover = tip .. close_color .. '_light.svg'
+theme.titlebar_close_button_focus_hover = tip .. close_color .. '_light.svg'
 
 -- Minimize Button
-theme.titlebar_minimize_button_normal = tip .. 'minimize_normal.svg'
-theme.titlebar_minimize_button_focus = tip .. 'minimize_focus.svg'
-
--- Ontop Button
-theme.titlebar_ontop_button_normal_inactive = tip .. 'ontop_normal_inactive.svg'
-theme.titlebar_ontop_button_focus_inactive = tip .. 'ontop_focus_inactive.svg'
-theme.titlebar_ontop_button_normal_active = tip .. 'ontop_normal_active.svg'
-theme.titlebar_ontop_button_focus_active = tip .. 'ontop_focus_active.svg'
-
--- Sticky Button
-theme.titlebar_sticky_button_normal_inactive =
-  tip .. 'sticky_normal_inactive.svg'
-theme.titlebar_sticky_button_focus_inactive = tip .. 'sticky_focus_inactive.svg'
-theme.titlebar_sticky_button_normal_active = tip .. 'sticky_normal_active.svg'
-theme.titlebar_sticky_button_focus_active = tip .. 'sticky_focus_active.svg'
+local minimize_color = 'yellow'
+theme.titlebar_minimize_button_normal = tip .. 'black_normal.svg'
+theme.titlebar_minimize_button_focus = tip .. minimize_color .. '_normal.svg'
+-- Hovered Minimize Buttin
+theme.titlebar_minimize_button_normal_hover =
+  tip .. minimize_color .. '_light.svg'
+theme.titlebar_minimize_button_focus_hover =
+  tip .. minimize_color .. '_light.svg'
 
 -- Floating Button
-theme.titlebar_floating_button_normal_inactive =
-  tip .. 'floating_normal_inactive.svg'
-theme.titlebar_floating_button_focus_inactive =
-  tip .. 'floating_focus_inactive.svg'
+local floating_color = 'blue'
+theme.titlebar_floating_button_normal_inactive = tip .. 'black_normal.svg'
+theme.titlebar_floating_button_focus_inactive = tip .. 'black_light.svg'
 theme.titlebar_floating_button_normal_active =
-  tip .. 'floating_normal_active.svg'
-theme.titlebar_floating_button_focus_active = tip .. 'floating_focus_active.svg'
+  tip .. floating_color .. '_normal.svg'
+theme.titlebar_floating_button_focus_active =
+  tip .. floating_color .. '_normal.svg'
+-- Hovered Floating Button
+theme.titlebar_floating_button_normal_inactive_hover = tip .. 'black_light.svg'
+theme.titlebar_floating_button_focus_inactive_hover =
+  tip .. floating_color .. '_light.svg'
+theme.titlebar_floating_button_normal_active_hover = tip .. 'black_light.svg'
+theme.titlebar_floating_button_focus_active_hover =
+  tip .. floating_color .. '_light.svg'
 
 -- Maximized Button
+local maximized_color = 'green'
 theme.titlebar_maximized_button_normal_inactive =
-  tip .. 'maximized_normal_inactive.svg'
+  tip .. 'black_normal.svg'
 theme.titlebar_maximized_button_focus_inactive =
-  tip .. 'maximized_focus_inactive.svg'
-theme.titlebar_maximized_button_normal_active =
-  tip .. 'maximized_normal_active.svg'
+  tip .. maximized_color .. '_normal.svg'
+theme.titlebar_maximized_button_normal_active = tip .. 'black_light.svg'
 theme.titlebar_maximized_button_focus_active =
-  tip .. 'maximized_focus_active.svg'
-
--- Hovered Close Button
-theme.titlebar_close_button_normal_hover = tip .. 'close_normal_hover.svg'
-theme.titlebar_close_button_focus_hover = tip .. 'close_focus_hover.svg'
-
--- Hovered Minimize Buttin
-theme.titlebar_minimize_button_normal_hover = tip .. 'minimize_normal_hover.svg'
-theme.titlebar_minimize_button_focus_hover = tip .. 'minimize_focus_hover.svg'
-
--- Hovered Ontop Button
-theme.titlebar_ontop_button_normal_inactive_hover =
-  tip .. 'ontop_normal_inactive_hover.svg'
-theme.titlebar_ontop_button_focus_inactive_hover =
-  tip .. 'ontop_focus_inactive_hover.svg'
-theme.titlebar_ontop_button_normal_active_hover =
-  tip .. 'ontop_normal_active_hover.svg'
-theme.titlebar_ontop_button_focus_active_hover =
-  tip .. 'ontop_focus_active_hover.svg'
-
--- Hovered Sticky Button
-theme.titlebar_sticky_button_normal_inactive_hover =
-  tip .. 'sticky_normal_inactive_hover.svg'
-theme.titlebar_sticky_button_focus_inactive_hover =
-  tip .. 'sticky_focus_inactive_hover.svg'
-theme.titlebar_sticky_button_normal_active_hover =
-  tip .. 'sticky_normal_active_hover.svg'
-theme.titlebar_sticky_button_focus_active_hover =
-  tip .. 'sticky_focus_active_hover.svg'
-
--- Hovered Floating Button
-theme.titlebar_floating_button_normal_inactive_hover =
-  tip .. 'floating_normal_inactive_hover.svg'
-theme.titlebar_floating_button_focus_inactive_hover =
-  tip .. 'floating_focus_inactive_hover.svg'
-theme.titlebar_floating_button_normal_active_hover =
-  tip .. 'floating_normal_active_hover.svg'
-theme.titlebar_floating_button_focus_active_hover =
-  tip .. 'floating_focus_active_hover.svg'
-
+  tip .. maximized_color .. '_normal.svg'
 -- Hovered Maximized Button
-theme.titlebar_maximized_button_normal_inactive_hover =
-  tip .. 'maximized_normal_inactive_hover.svg'
+theme.titlebar_maximized_button_normal_inactive_hover = tip .. maximized_color .. '_light.svg'
 theme.titlebar_maximized_button_focus_inactive_hover =
-  tip .. 'maximized_focus_inactive_hover.svg'
-theme.titlebar_maximized_button_normal_active_hover =
-  tip .. 'maximized_normal_active_hover.svg'
+  tip .. maximized_color .. '_light.svg'
+theme.titlebar_maximized_button_normal_active_hover = tip .. 'black_light.svg'
 theme.titlebar_maximized_button_focus_active_hover =
-  tip .. 'maximized_focus_active_hover.svg'
+  tip .. maximized_color .. '_light.svg'
+
+-- Ontop Button
+local ontop_color = 'cyan'
+theme.titlebar_ontop_button_normal_inactive = tip .. 'black_normal.svg'
+theme.titlebar_ontop_button_focus_inactive = tip .. 'black_light.svg'
+theme.titlebar_ontop_button_normal_active = tip .. ontop_color .. '_normal.svg'
+theme.titlebar_ontop_button_focus_active = tip .. ontop_color .. '_normal.svg'
+-- Hovered Ontop Button
+theme.titlebar_ontop_button_normal_inactive_hover = tip .. 'black_light.svg'
+theme.titlebar_ontop_button_focus_inactive_hover =
+  tip .. ontop_color .. '_light.svg'
+theme.titlebar_ontop_button_normal_active_hover = tip .. 'black_light.svg'
+theme.titlebar_ontop_button_focus_active_hover =
+  tip .. ontop_color .. '_light.svg'
+
+-- Sticky Button
+local sticky_color = 'magenta'
+theme.titlebar_sticky_button_normal_inactive = tip .. 'black_normal.svg'
+theme.titlebar_sticky_button_focus_inactive = tip .. 'black_light.svg'
+theme.titlebar_sticky_button_normal_active = tip .. sticky_color .. '_normal.svg'
+theme.titlebar_sticky_button_focus_active = tip .. sticky_color .. '_normal.svg'
+-- Hovered Sticky Button
+theme.titlebar_sticky_button_normal_inactive_hover = tip .. 'black_light.svg'
+theme.titlebar_sticky_button_focus_inactive_hover = tip .. sticky_color .. '_light.svg'
+theme.titlebar_sticky_button_normal_active_hover = tip .. 'black_light.svg'
+theme.titlebar_sticky_button_focus_active_hover = tip .. sticky_color .. '_light.svg'
 
 -- UI Groups
 
